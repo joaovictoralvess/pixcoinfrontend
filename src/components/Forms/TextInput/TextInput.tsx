f0f18eabc444bc6375c32d83db99d4e3b24e4d9b
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-11-27 14:52:28 -0300

    feat: permite mensagem de erro no text input

--- src/components/Forms/TextInput/TextInput.tsx
+++ src/components/Forms/TextInput/TextInput.tsx
@@ -6,15 +6,23 @@ export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
 	name: string;
 	label: string;
 	icon?: ReactNode;
+	error?: string;
 }
 
-export default function TextInput({ name, label, icon, ...rest }: TextInputProps) {
+export default function TextInput({
+	name,
+	label,
+	icon,
+	error,
+	...rest
+}: TextInputProps) {
 	return (
 		<div className='text-input-container'>
-			<input id={name} {...rest} />
+			<input name={name} id={name} {...rest} />
 			<label htmlFor={name}>{label}</label>
 			<span className='bar'></span>
 			{icon && (<span className='icon'>{icon}</span>)}
+			{error && <span className='error'>{error}</span>}
 		</div>
 	)
 }
