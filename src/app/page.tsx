68fac061c16d88882be185e687e9660bad85b61e
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-11-27 16:28:08 -0300

    feat: redireciona cliente para login caso não esteja autenticado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-export default function Home() {
-	return (
-		<h1>Home</h1>
-	);
+import { getSession } from '@/helpers/session';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+	const user = await getSession();
+	if (!user) {
+		redirect('/auth/customer/sign-in');
+	}
+
+	return null;
 }
