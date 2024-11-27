4bb066ad0e7bb29af215f17a44f30b3827c3d836
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-11-27 14:32:27 -0300

    feat: permite ícones no text input

--- src/components/Forms/TextInput/TextInput.tsx
+++ src/components/Forms/TextInput/TextInput.tsx
@@ -1,18 +1,20 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, ReactNode } from 'react';
 
 import './styles.scss';
 
 export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
 	name: string;
 	label: string;
+	icon?: ReactNode;
 }
 
-export default function TextInput({ name, label, ...rest }: TextInputProps) {
+export default function TextInput({ name, label, icon, ...rest }: TextInputProps) {
 	return (
 		<div className='text-input-container'>
 			<input id={name} {...rest} />
 			<label htmlFor={name}>{label}</label>
 			<span className='bar'></span>
+			{icon && (<span className='icon'>{icon}</span>)}
 		</div>
 	)
 }
