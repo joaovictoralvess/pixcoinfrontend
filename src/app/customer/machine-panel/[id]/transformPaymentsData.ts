d418016d9f308d4f3422e5206255df304f9b7c3b
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-11-28 01:32:29 -0300

    feat: cria helper para informações de pagamento

--- src/app/customer/machine-panel/[id]/transformPaymentsData.ts
+++ src/app/customer/machine-panel/[id]/transformPaymentsData.ts
@@ -1,5 +1,6 @@
 import { IPayment } from '@/interfaces/IPayment';
 import { TableData } from '@/app/customer/machine-panel/components/Table/Table';
+import { formatToBRL, retrieveFormattedDate, retrievePaymentForm, retrieveReversedText } from '@/helpers/payment';
 
 export default function transformPaymentsData(payments: IPayment[]): TableData[] {
 	return payments.map(payment => ({
@@ -12,47 +13,3 @@ export default function transformPaymentsData(payments: IPayment[]): TableData[]
 	}));
 }
 
-const retrieveFormattedDate = (isoDate: string): string => {
-	const date = new Date(isoDate);
-
-	const day = String(date.getDate()).padStart(2, '0');
-	const month = String(date.getMonth() + 1).padStart(2, '0'); // `getMonth()` retorna de 0 a 11
-	const year = date.getFullYear();
-
-	const hours = String(date.getHours()).padStart(2, '0');
-	const minutes = String(date.getMinutes()).padStart(2, '0');
-	const seconds = String(date.getSeconds()).padStart(2, '0');
-
-	return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-};
-
-const retrievePaymentForm = (currentPaymentForm: string): string => {
-	switch (currentPaymentForm) {
-		case 'bank_transfer':
-			return 'PIX';
-		case 'CASH':
-			return 'Especie';
-		case 'debit_card':
-			return 'Débito';
-		case 'credit_card':
-			return 'Crédito';
-		default:
-			return ''
-	}
-};
-
-const formatToBRL = (value: string): string => {
-	const number = parseFloat(value);
-	return new Intl.NumberFormat('pt-BR', {
-		style: 'currency',
-		currency: 'BRL',
-	}).format(number);
-}
-
-const retrieveReversedText = (reversed: boolean): 'Recebido' | 'Estornado' => {
-	if (reversed) {
-		return 'Estornado';
-	}
-
-	return 'Recebido'
-}
---
+++ src/helpers/payment.ts
@@ -0,0 +1,44 @@
+export const retrieveFormattedDate = (isoDate: string): string => {
+	const date = new Date(isoDate);
+
+	const day = String(date.getDate()).padStart(2, '0');
+	const month = String(date.getMonth() + 1).padStart(2, '0'); // `getMonth()` retorna de 0 a 11
+	const year = date.getFullYear();
+
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const seconds = String(date.getSeconds()).padStart(2, '0');
+
+	return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+};
+
+export const retrievePaymentForm = (currentPaymentForm: string): string => {
+	switch (currentPaymentForm) {
+		case 'bank_transfer':
+			return 'PIX';
+		case 'CASH':
+			return 'Especie';
+		case 'debit_card':
+			return 'Débito';
+		case 'credit_card':
+			return 'Crédito';
+		default:
+			return ''
+	}
+};
+
+export const formatToBRL = (value: string): string => {
+	const number = parseFloat(value);
+	return new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+	}).format(number);
+}
+
+export const retrieveReversedText = (reversed: boolean): 'Recebido' | 'Estornado' => {
+	if (reversed) {
+		return 'Estornado';
+	}
+
+	return 'Recebido'
+}
