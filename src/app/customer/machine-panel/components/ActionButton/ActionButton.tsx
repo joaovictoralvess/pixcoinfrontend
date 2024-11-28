9c020237a3a39cbb1ced89f0fafa59d69aee06d8
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-11-28 18:36:19 -0300

    feat: permite callback em ActionButton

--- src/app/customer/machine-panel/components/ActionButton/ActionButton.tsx
+++ src/app/customer/machine-panel/components/ActionButton/ActionButton.tsx
@@ -7,14 +7,26 @@ import { redirect } from 'next/navigation';
 import './styles.scss';
 
 export interface ActionButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	updateTo: string;
+	updateTo?: string;
 	icon: ReactNode
 	className?: string;
+	callback?: () => void;
 }
 
-export default function ActionButton({ updateTo, icon, className, children,  ...rest }: ActionButtonProps) {
+export default function ActionButton({ updateTo, icon, className, children, callback,  ...rest }: ActionButtonProps) {
+
+	const handleClick = () => {
+		if (updateTo) {
+			redirect(updateTo);
+		}
+
+		if (callback !== undefined) {
+			callback();
+		}
+	}
+
 	return (
-		<button onClick={() => redirect(updateTo)} className={`action-button ${className ? className : ''}`} {...rest}>
+		<button onClick={() => handleClick()} className={`action-button ${className ? className : ''}`} {...rest}>
 			{icon}
 			{children}
 		</button>
