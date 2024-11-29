93b2962ca49c4fabc508973d1b1f8963a674127e
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-11-29 10:34:17 -0300

    refactor: remove comentário

--- src/helpers/payment.ts
+++ src/helpers/payment.ts
@@ -2,7 +2,7 @@ export const retrieveFormattedDate = (isoDate: string): string => {
 	const date = new Date(isoDate);
 
 	const day = String(date.getDate()).padStart(2, '0');
-	const month = String(date.getMonth() + 1).padStart(2, '0'); // `getMonth()` retorna de 0 a 11
+	const month = String(date.getMonth() + 1).padStart(2, '0');
 	const year = date.getFullYear();
 
 	const hours = String(date.getHours()).padStart(2, '0');
