ccdf25e049632376012d67f4f45eafc5c9c1bd95
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-12-11 00:46:37 -0300

    feat: redireciona usuário para login ou home conforme o papel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { getSession } from '@/helpers/session';
 import { redirect } from 'next/navigation';
+import { User } from '@/interfaces/User';
 
 export default async function Home() {
-	const user = await getSession();
+	const user = await getSession() as User;
 	if (!user) {
 		redirect('/auth/customer/sign-in');
 	}
 
-	return null;
+	const isAdmin = user.key === 'ADMIN';
+	if (isAdmin) {
+		redirect('/admin/customers');
+	}
+
+	redirect('/customer/machine-panel');
 }
