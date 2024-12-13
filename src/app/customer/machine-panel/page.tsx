5b4e42005c135e9b5a962b15be572a278625b6f0
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2024-12-13 01:45:50 -0300

    feat: exibe modal de boas vindas

--- src/app/customer/machine-panel/page.tsx
+++ src/app/customer/machine-panel/page.tsx
@@ -2,8 +2,12 @@ import Header from '@/components/UI/Header/Header';
 import Layout from '@/components/UI/Layout/Layout';
 import PageTitleWithSync from '@/components/UI/PageTitleWithSync/PageTitleWithSync';
 import Machine from '@/components/UI/Machine/Machine';
+import WellComeCustomer from '@/components/UI/WellcomeCustomer/WellComecustomer';
 
+import { getSession } from '@/helpers/session';
 import { redirectCustomerToLoginIfNotLogged } from '@/helpers/customer';
+
+import { User } from '@/interfaces/User';
 
 import MachineService from '@/services/Machine';
 
@@ -11,11 +15,13 @@ import './styles.scss';
 
 export default async function MachinePanel() {
 	await redirectCustomerToLoginIfNotLogged()
+	const user = await getSession() as User;
 
 	const machines = await MachineService.all();
 
 	return (
 		<>
+			<WellComeCustomer name={user.name} />
 			<Header />
 			<main className='machine-panel'>
 				<Layout className='machine-panel__container'>
---
+++ src/components/UI/WellcomeCustomer/WellComecustomer.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import Button from '@/components/Forms/Button/Button';
+import Modal from '@/components/UI/Modal/Modal';
+
+export interface WellComeCustomerProps {
+	name: string;
+}
+
+import './styles.scss';
+
+export default function WellComeCustomer({ name }: WellComeCustomerProps) {
+	const [open, setOpen] = useState(true);
+	const [shouldShow, setShouldShow] = useState(false);
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			const showModal = sessionStorage.getItem('show-wellcome-modal');
+			if (showModal !== '0') {
+				setShouldShow(true);
+			}
+		}
+	}, []);
+
+	const handleCloseAndMarkNotShowAgain = () => {
+		sessionStorage.setItem('show-wellcome-modal', '0');
+		setOpen(false);
+		setShouldShow(false);
+	};
+
+	if (!shouldShow || !open) {
+		return null;
+	}
+
+	return (
+		<Modal onClose={() => setOpen(!open)} title="Bem Vindo!">
+			<div className="wellcome-text">
+				<p>
+					Olá, {name}!
+					Que alegria ter você conosco! É uma honra enorme apresentar a você o nosso novo site, feito com muito carinho
+					para oferecer uma experiência ainda mais incrível.
+				</p>
+
+				<p>
+					Estamos sempre buscando evoluir para melhor atendê-lo, e este é apenas o começo de uma jornada repleta de
+					novidades. Navegue à vontade e conte com a gente para o que precisar!
+				</p>
+
+				<p>
+					Seja bem-vindo(a) ao novo capítulo da nossa história.
+				</p>
+
+				<p>
+					Abraços,
+					Equipe Pixcoin
+				</p>
+			</div>
+
+			<div className="wellcome-buttons">
+				<Button onClick={() => setOpen(false)}>Fechar</Button>
+				<Button onClick={handleCloseAndMarkNotShowAgain}>Fechar e não exibir novamente</Button>
+			</div>
+		</Modal>
+	);
+}
