40f5422a7f77d0220875281070c0e4b982b78f6d
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-03-22 19:28:46 -0300

    Permite adicionar a mensagem para todos os clientes

--- src/app/admin/customers/components/AddWarningForm/action.ts
+++ src/app/admin/customers/components/AddWarningForm/action.ts
@@ -7,11 +7,12 @@ import { redirect } from 'next/navigation';
 export const AddWarningToClient = async (_: any, formData: FormData) => {
 	const customerId = `${formData.get('id')}`;
 	const message = `${formData.get('message')}`;
+	const showForAll = `${formData.get('showForAll')}`;
 	if (!customerId) {
 		return;
 	}
 
-	const resp = await AdminService.addWarningToCustomer(customerId, message);
+	const resp = await AdminService.addWarningToCustomer(customerId, message, showForAll);
 
 	if (resp.message) {
 		revalidatePath(`/admin/customers/${customerId}`);
