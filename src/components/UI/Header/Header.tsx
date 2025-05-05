fdfb7baaec1f1cbe37054d298e940d88372f74db
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-05-05 10:19:22 -0300

    Cria sidebar WIP

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -6,6 +6,8 @@ import { User } from '@/interfaces/User';
 
 import { signOut } from '@/app/auth/customer/sign-out/actions';
 
+import Sidebar from '@/components/UI/Sidebar/Sidebar';
+
 import './styles.scss';
 
 export interface HeaderProps {
@@ -16,37 +18,41 @@ export default async function Header({ iconLeft }: Readonly<HeaderProps>) {
 	const user = (await getSession()) as User;
 
 	return (
-		<header className="header">
-			<div className="header__title-wrapper">
-				<button className="header__title-wrapper__burger-btn">
-					<span className="header__title-wrapper__burger-btn__line"></span>
-				</button>
+		<>
+			<header className="header">
+				<div className="header__title-wrapper">
+					<button className="header__title-wrapper__burger-btn">
+						<span className="header__title-wrapper__burger-btn__line"></span>
+					</button>
 
-				<div className="header__title-wrapper-logo">
-					<span className="header__title-wrapper-logo__logo">PIXcoin</span>
-					<span className="header__title-wrapper-logo__customer">
-					Olá, {user.name} {generateRandomEmoji()}
-				</span>
+					<div className="header__title-wrapper-logo">
+						<span className="header__title-wrapper-logo__logo">PIXcoin</span>
+						<span className="header__title-wrapper-logo__customer">
+							Olá, {user.name} {generateRandomEmoji()}
+						</span>
+					</div>
 				</div>
-			</div>
 
-			<nav className="header__right">
-				<ul className="header__right__list">
-					{iconLeft && (
-						<li className="header__right__list__item">{iconLeft}</li>
-					)}
+				<nav className="header__right">
+					<ul className="header__right__list">
+						{iconLeft && (
+							<li className="header__right__list__item">{iconLeft}</li>
+						)}
 
-					<li className="header__right__list__item">
-						<button
-							onClick={signOut}
-							type="button"
-							className="header__right__list__item__button"
-						>
-							Sair
-						</button>
-					</li>
-				</ul>
-			</nav>
-		</header>
+						<li className="header__right__list__item">
+							<button
+								onClick={signOut}
+								type="button"
+								className="header__right__list__item__button"
+							>
+								Sair
+							</button>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+			<Sidebar />
+		</>
 	);
 }
---
+++ src/components/UI/Sidebar/Sidebar.tsx
@@ -0,0 +1,7 @@
+import './styles.scss';
+
+export default async function Sidebar() {
+	return (
+		<aside className="sidebar"></aside>
+	)
+}
